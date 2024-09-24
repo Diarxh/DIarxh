edc3f04eaa14627c0d9b56e84bbdeabe39075db4
@akib8730 <akib8730@gmail.com>
2024-09-24 11:13:14 +0700

    Deskripsi perubahan Anda

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/about', [\App\Http\Controllers\HomeController::class, 'about']);
